1fce3691b550e9d0e56586431de5dac1ed214b50
Ahmad ElSayed <aelsayed1226@gmail.com>
2026-05-07 12:24:36 +1000

    version negotiation update

--- src/store/version-info/reducer.js
+++ src/store/version-info/reducer.js
@@ -5,9 +5,11 @@ import {
   SET_VERSIONS_READ_ONLY
 } from './actions'
 
-export default function versionInfo(state={vHeaders: {xV: '999', xMinV: '1'}}, action) {
+const DEFAULT_XV = '999'
+
+export default function versionInfo(state={vHeaders: {xV: DEFAULT_XV, xMinV: '1'}}, action) {
   const vHeaders = {
-    xV: loadVersionField("x-v") || state.vHeaders.xV,
+    xV: DEFAULT_XV,
     xMinV: loadVersionField("x-min-v") || state.vHeaders.xMinV
   }
   switch (action.type) {
@@ -15,7 +17,6 @@ export default function versionInfo(state={vHeaders: {xV: '999', xMinV: '1'}}, a
       return {vHeaders}
     case SAVE_VERSION_INFO:
       const vi = action.versionInfo
-      saveVersionField("x-v", vi.xV)
       saveVersionField("x-min-v", vi.xMinV)
       return {vHeaders: vi}
     case SET_VERSIONS_EDITABLE:
@@ -27,15 +28,8 @@ export default function versionInfo(state={vHeaders: {xV: '999', xMinV: '1'}}, a
   }
 }
 
-const DEPRECATED_VERSIONS = ['4']
-
 function loadVersionField(vf) {
-  const val = window.localStorage.getItem(vf)
-  if (vf === 'x-v' && DEPRECATED_VERSIONS.includes(val)) {
-    window.localStorage.removeItem(vf)
-    return null
-  }
-  return val
+  return window.localStorage.getItem(vf)
 }
 
 function saveVersionField(vf, value) {
